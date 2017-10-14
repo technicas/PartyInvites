0e7de1fc32829e519885bd3e88776b7b2fc367f0
technicas <technicas@qq.com>
2017-10-15 00:15:44 +0800

    模块 PartyInvites 的初始签入

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Phone
+        {
+            get;
+            set;
+        }
+
+        public bool? WillAttend
+        {
+            get;
+            set;
+        }
+
+        public GuestResponse()
+        {
+        }
+    }
+}
